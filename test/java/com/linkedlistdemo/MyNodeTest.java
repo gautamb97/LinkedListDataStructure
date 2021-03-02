854c6adc1fc7c29fa2ce26130ddffbf7f592da00
gautamb97 <www.gautam971997@gmail.com>
2021-03-02 20:31:16 +0530

    Added:Created simple LinkedList with three values

--- test/java/com/linkedlistdemo/MyNodeTest.java
+++ test/java/com/linkedlistdemo/MyNodeTest.java
@@ -1,6 +1,19 @@
 package com.linkedlistdemo;
 
 
-public class MyNodeTest {
+import org.junit.Assert;
+import org.junit.Test;
 
+public class MyNodeTest {
+    @Test
+    public void given3NumbersWhenLinkedShouldPassedLinkedListTest(){
+        MyNode<Integer> myFirstNode = new MyNode<Integer>(56);
+        MyNode<Integer> mySecondNode = new MyNode<Integer>(30);
+        MyNode<Integer> myThirdNode = new MyNode<Integer>(70);
+        myFirstNode.setNext(mySecondNode);
+        mySecondNode.setNext(myThirdNode);
+        boolean result = myFirstNode.getNext().equals(mySecondNode) &&
+                         mySecondNode.getNext().equals(myThirdNode);
+        Assert.assertTrue(result);
+    }
 }
