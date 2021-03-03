31b35076040b4221816072086fabe76573806138
gautamb97 <www.gautam971997@gmail.com>
2021-03-03 16:02:50 +0530

    Added:Created liskedlist and added three values using add method

---
+++ test/java/com/linkedlistdemo/MyLinkedListTest.java
@@ -0,0 +1,22 @@
+package com.linkedlistdemo;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MyLinkedListTest {
+    @Test
+    public void given3NumbersWhenAddedToLinkedListShouldBeAddedToTop(){
+        MyNode<Integer> myFirstNode = new MyNode<Integer>(70);
+        MyNode<Integer> mySecondNode = new MyNode<Integer>(30);
+        MyNode<Integer> myThirdNode = new MyNode<Integer>(56);
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.add(myFirstNode);
+        myLinkedList.add(mySecondNode);
+        myLinkedList.add(myThirdNode);
+        myLinkedList.printMyNodes();
+        boolean result = myLinkedList.head.equals(myThirdNode) &&
+                         myLinkedList.head.getNext().equals(mySecondNode) &&
+                         myLinkedList.tail.equals(myFirstNode);
+        Assert.assertTrue(result);
+    }
+}
