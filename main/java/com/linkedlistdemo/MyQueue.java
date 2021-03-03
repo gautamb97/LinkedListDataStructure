ae79787846eb7f6d965af884d79015b38872525b
gautamb97 <www.gautam971997@gmail.com>
2021-03-03 21:07:22 +0530

    Added:Created a queue and added elements to it

---
+++ main/java/com/linkedlistdemo/MyQueue.java
@@ -0,0 +1,24 @@
+package com.linkedlistdemo;
+
+public class MyQueue {
+    private final MyLinkedList myLinkedList;
+
+    public MyQueue(){
+        this.myLinkedList = new MyLinkedList();
+    }
+    public void enqueue(MyNode myNode) {
+        this.myLinkedList.append(myNode);
+    }
+
+    public void printQueue() {
+        this.myLinkedList.printMyNodes();
+    }
+
+    public INode getHead() {
+        return this.myLinkedList.head;
+    }
+
+    public INode getTail() {
+        return this.myLinkedList.tail;
+    }
+}
---
+++ test/java/com/linkedlistdemo/MyQueueTest.java
@@ -0,0 +1,22 @@
+package com.linkedlistdemo;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MyQueueTest {
+    @Test
+    public void given3NumbersWhenAddedShouldReturnAnEnqueue(){
+        MyNode<Integer> myFirstNode=new MyNode<>(56);
+        MyNode<Integer> mySecondNode=new MyNode<>(30);
+        MyNode<Integer> myThirdNode=new MyNode<>(70);
+        MyQueue myQueue = new MyQueue();
+        myQueue.enqueue(myFirstNode);
+        myQueue.enqueue(mySecondNode);
+        myQueue.enqueue(myThirdNode);
+        myQueue.printQueue();
+        boolean result= myQueue.getHead().equals(myFirstNode)&&
+                myQueue.getHead().getNext().equals(mySecondNode)&&
+                myQueue.getTail().equals(myThirdNode);
+        Assert.assertTrue(result);
+    }
+}
